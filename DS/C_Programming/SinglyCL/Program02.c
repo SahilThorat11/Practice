b2acb60636ac7556d23ce01b47f889cd8f6254cf
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-20 15:07:37 +0530

    InsertFirst Function

---
+++ DS/C_Programming/SinglyCL/Program02.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data; 
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, PPNODE last, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if((*first == NULL) && (*last == NULL))
+    {
+        *first = newn;
+        *last = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+
+    (*last) -> next = *first;
+}
+
+void InsertLast(PPNODE first, PPNODE last, int no)
+{}
+
+void DeleteFirst(PPNODE first, PPNODE last)
+{}
+
+void DeleteLast(PPNODE first, PPNODE last)
+{}
+
+void Display(PNODE first, PNODE last)
+{}
+
+void Count(PNODE first, PNODE last)
+{}
+
+void InsertAtPos(PPNODE first, PPNODE last, int no, int iPos)
+{}
+
+void DeleteAtPos(PPNODE first, PPNODE last, int iPos)
+{}
+
+int main()
+{
+    PNODE head = NULL;
+    PNODE tail = NULL;
+
+    return 0;
+}   
