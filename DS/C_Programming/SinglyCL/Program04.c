a8c366c0a0cea316e9e1ef1ffc2fa4a3a2b1692e
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-20 15:15:14 +0530

    Display Function

---
+++ DS/C_Programming/SinglyCL/Program04.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data; 
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, PPNODE last, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if((*first == NULL) && (*last == NULL))
+    {
+        *first = newn;
+        *last = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+
+    (*last) -> next = *first;
+}
+
+void InsertLast(PPNODE first, PPNODE last, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if((*first == NULL) && (*last == NULL))
+    {
+        *first = newn;
+        *last = newn;
+    }
+    else
+    {
+        (*last) -> next = newn;
+        *last = newn;
+    }
+
+    (*last) -> next = *first;
+}
+
+void DeleteFirst(PPNODE first, PPNODE last)
+{}
+
+void DeleteLast(PPNODE first, PPNODE last)
+{}
+
+void Display(PNODE first, PNODE last)
+{
+    printf("\n -> ");
+    do
+    {
+        printf("| %d | -> ", first -> data);
+        first = first -> next;
+    } while (first != last -> next);
+
+    printf("\n\n");
+}
+
+void Count(PNODE first, PNODE last)
+{}
+
+void InsertAtPos(PPNODE first, PPNODE last, int no, int iPos)
+{}
+
+void DeleteAtPos(PPNODE first, PPNODE last, int iPos)
+{}
+
+int main()
+{
+    PNODE head = NULL;
+    PNODE tail = NULL;
+
+    return 0;
+}   
