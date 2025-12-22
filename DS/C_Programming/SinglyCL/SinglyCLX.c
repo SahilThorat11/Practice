4f5457c4e78b94450feac9001e396fca56d978c9
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-22 12:41:43 +0530

    DeleteFirst function without temporary pointer

---
+++ DS/C_Programming/SinglyCL/SinglyCLX.c
@@ -0,0 +1,266 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, PPNODE last, int iNO)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNO;
+    newn -> next = NULL;
+
+    if((*first == NULL) && (*last == NULL))
+    {
+        *first = newn;
+        *last = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+
+    (*last) -> next = *first;
+}
+
+void InsertLast(PPNODE first, PPNODE last, int iNO)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNO;
+    newn -> next = NULL;
+
+    if((*first == NULL) && (*last == NULL))
+    {
+        *first = newn;
+        *last = newn;
+    }
+    else
+    {
+        (*last) -> next = newn;
+        *last = newn;
+    }
+
+    (*last) -> next = *first;
+}
+
+void DeleteFirst(PPNODE first, PPNODE last)   // Without Using Temporary pointer
+{
+    if((*first == NULL) && (*last == NULL))
+    {
+        return;
+    }
+    else if(*first == *last)
+    {
+        free(*first);
+        *first = NULL;
+        *last = NULL;
+    }
+    else
+    {
+        *first = (*first) -> next;
+        free((*last) -> next);
+        
+        (*last) -> next = *first;
+    }
+}
+
+void DeleteLast(PPNODE first, PPNODE last)
+{
+    PNODE temp = NULL;
+
+    if((*first == NULL) && (*last == NULL))
+    {
+        return;
+    }
+    else if(*first == *last)
+    {
+        free(*first);
+        *first = NULL;
+        *last = NULL;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp -> next -> next != (*last) -> next)
+        {
+            temp = temp -> next;
+        } 
+
+        free(temp -> next);
+        *last = temp;
+
+        (*last) -> next = *first;
+    }
+}
+
+void Display(PNODE first, PNODE last)
+{
+    printf("\n -> ");
+    do 
+    {
+        printf("| %d | -> ", first -> data);
+        first = first -> next;
+    }while(first != last -> next);
+
+    printf("\n\n");
+}
+
+int Count(PNODE first, PNODE last)
+{
+    int iCount = 0;
+
+    do 
+    {
+        iCount++;
+        first = first -> next;
+    }while(first != last -> next);
+
+    return iCount;
+}
+
+void InsertAtPos(PPNODE first, PPNODE last, int iNO, int iPos)
+{
+    int iSize = 0;
+    int iCnt = 0;
+
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    iSize = Count(*first, *last);
+
+    if((iPos < 1) || (iPos > iSize + 1))
+    {
+        printf("Invalid Position");
+        return;
+    }
+
+    if(iPos == 1)
+    {
+        InsertFirst(first, last, iNO);
+    }
+    else if(iPos == iSize + 1)
+    {
+        InsertLast(first, last, iNO);
+    }
+    else
+    {
+        newn = (PNODE)malloc(sizeof(NODE));
+
+        newn -> data = iNO;
+        newn -> next = NULL;
+
+        temp = *first;
+
+        for(iCnt = 1; iCnt < iPos - 1; iCnt++)
+        {
+            temp = temp -> next;
+        }
+
+        newn -> next = temp -> next;
+        temp -> next = newn;
+    }
+}
+
+void DeleteAtPos(PPNODE first, PPNODE last, int iPos)
+{
+    int iSize = 0;
+    int iCnt = 0;
+
+    PNODE target = NULL;
+    PNODE temp = NULL;
+
+    iSize = Count(*first, *last);
+
+    if((iPos < 1) || (iPos > iSize))
+    {
+        printf("Invalid Position");
+        return;
+    }
+
+    if(iPos == 1)
+    {
+        DeleteFirst(first, last);
+    }
+    else if(iPos == iSize)
+    {
+        DeleteLast(first, last);
+    }
+    else
+    {
+        temp = *first;
+
+        for(iCnt = 1; iCnt < iPos - 1; iCnt++)
+        {
+            temp = temp -> next;
+        }
+
+        target = temp -> next;
+
+        temp -> next = target -> next;
+        free(target);
+    }
+}
+
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+    PNODE tail = NULL;
+
+    InsertFirst(&head, &tail, 51);
+    InsertFirst(&head, &tail, 21);
+    InsertFirst(&head, &tail, 11);
+
+    Display(head, tail);
+    iRet = Count(head, tail);
+    printf("Number of nodes are : %d\n", iRet);
+
+    InsertLast(&head, &tail, 101);
+    InsertLast(&head, &tail, 111);
+    InsertLast(&head, &tail, 121);
+
+    Display(head, tail);
+    iRet = Count(head, tail);
+    printf("Number of nodes are : %d\n", iRet);
+
+    InsertAtPos(&head, &tail, 75, 4);
+
+    Display(head, tail);
+    iRet = Count(head, tail);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteAtPos(&head, &tail, 4);
+
+    Display(head, tail);
+    iRet = Count(head, tail);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteFirst(&head, &tail);
+
+    Display(head, tail);
+    iRet = Count(head, tail);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteLast(&head, &tail);
+
+    Display(head, tail);
+    iRet = Count(head, tail);
+    printf("Number of nodes are : %d\n", iRet);
+
+    return 0;
+}
