5b2820e2022214a81f1d989f949272b3f0b3a7b0
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-23 12:02:44 +0530

    Display Function

---
+++ DS/C_Programming/DoublyCL/Program03.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+    struct node * prev;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsesrtFirst(PPNODE first, PPNODE last, int iNo)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+    newn -> prev = NULL;
+
+    if((*first == NULL) && (*last == NULL))
+    {
+        *first = newn;
+        *last = newn;
+    }
+    else
+    {
+        newn -> next = (*first);
+        (*first) -> prev = newn;
+        *first = newn;
+    }
+
+    (*last) -> next = *first;
+    (*first) -> prev = *last;
+}
+
+void InsesrtLast(PPNODE first, PPNODE last, int iNo)
+{}
+
+void DeleteFirst(PPNODE first, PPNODE last)
+{}
+
+void DeleteLast(PPNODE first, PPNODE last)
+{}
+
+void Display(PNODE first, PNODE last)
+{
+    if((first == NULL) && (last == NULL))
+    {
+        printf("Linked List is empty");
+        return;
+    }
+
+    printf("\n <=> ");
+
+    do 
+    {
+        printf("| %d | <=> ", first -> data);
+        first = first -> next;
+    }while(first != last -> next);
+
+    printf("\n\n");
+}
+
+int Count(PNODE first, PNODE last)
+{
+    return 0;
+}
+
+void InsesrtAtPos(PPNODE first, PPNODE last, int iNo, int iPos)
+{}
+
+void DeleteAtPos(PPNODE first, PPNODE last)
+{}
+
+int main()
+{
+    PNODE head = NULL;
+    PNODE tail = NULL;
+
+    InsesrtFirst(&head, &tail, 51);
+    InsesrtFirst(&head, &tail, 21);
+    InsesrtFirst(&head, &tail, 11);
+
+    Display(head, tail);
+
+    return 0;
+}
