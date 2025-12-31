88da6a4d9f0b3e4b0c5a57ad698ebe3b3a23f9ee
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-31 12:11:33 +0530

    InsertLast Function

---
+++ DS/C++_Programming/DoublyLL/Program03.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node * next;
+    struct node * prev;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+
+class DoublyLL
+{
+    private:
+        PNODE first;
+        int iCount;
+
+    public:
+
+        DoublyLL()
+        {
+            first = NULL;
+            iCount = 0;
+        }
+
+        void InsertFirst(int no)
+        {
+            PNODE newn = NULL;
+
+            newn = new NODE;
+
+            newn -> data = no;
+            newn -> next = NULL;
+            newn -> prev = NULL;
+
+            if(first == NULL)
+            {
+                first = newn;
+            }
+            else
+            {
+                newn -> next = first;
+                first->prev = newn;
+                first = newn;
+            }
+
+            iCount++;
+        }
+
+        void InsertLast(int no)
+        {
+            PNODE newn = NULL;
+            PNODE temp = NULL;
+
+            newn = new NODE;
+
+            newn -> data = no;
+            newn -> next = NULL;
+            newn -> prev = NULL;
+
+            if(first == NULL)
+            {
+                first = newn;
+            }
+            else
+            {
+                temp = first;
+
+                while(temp -> next != NULL)
+                {
+                    temp = temp -> next;
+                }
+
+                temp -> next = newn;
+                newn -> prev = temp;
+            }
+
+            iCount++;
+        }
+
+        void InsertAtPos(int no, int iPos)
+        {}
+
+        void DeleteFirst()
+        {}
+
+        void DeleteLast()
+        {}
+
+        void DeleteAtPos(int iPos)
+        {}
+
+        void Display()
+        {
+            PNODE temp = NULL;
+
+            temp = first;
+
+            cout<<"NULL <=> ";
+
+            while(temp != NULL)
+            {
+                cout<<"| "<<temp -> data<<" | <=> ";
+                temp = temp -> next;
+            }
+
+            cout<<"NULL\n";
+        }
+
+        int Count()
+        {
+            return iCount;
+        }
+};
+
+int main()
+{
+    int iRet = 0;
+    DoublyLL obj;
+
+    obj.InsertFirst(51);
+    obj.InsertFirst(21);
+    obj.InsertFirst(11);
+
+    obj.Display();
+    iRet = obj.Count();
+    cout<<"Number of nodes are : "<<iRet<<"\n";
+
+    obj.InsertLast(101);
+    obj.InsertLast(111);
+    obj.InsertLast(121);
+
+    obj.Display();
+    iRet = obj.Count();
+    cout<<"Number of nodes are : "<<iRet<<"\n";
+
+    return 0;
+}
