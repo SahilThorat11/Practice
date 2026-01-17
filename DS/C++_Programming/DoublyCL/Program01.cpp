420124184f2de3b010fa97f706c6f1c3a09808b7
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-17 13:00:40 +0530

    InsertFirst Function

---
+++ DS/C++_Programming/DoublyCL/Program01.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+
+class DoublyCL
+{
+    public :
+        PNODE first;
+        PNODE last;
+        int iCount;
+
+        DoublyLL()
+        {
+            first = NULL;
+            last = NULL;
+            iCount = 0;
+        }
+
+        void InsertFirst(int no)
+        {
+            PNODE newn = NULL;
+
+            newn = new NODE;
+
+            newn -> data = no;
+            newn -> next = NULL;
+            newn -> prev = NULL;
+            
+            if((first == NULL) && (last == NULL))   // LL is empty
+            {
+                first = newn;
+                last = newn;
+            }
+            else                                    // LL contains more than one node
+            {
+                newn -> next = first;
+                first -> prev = newn;
+                first = newn;
+            }
+
+            last -> next = first;
+            first -> prev = last;
+
+            iCount++;
+        }
+
+        void InsertLast(int no)
+        {}
+
+        void InsertAtPos(int no)
+        {}
+
+        void DeleteFirst()
+        {}
+
+        void DeleteLast()
+        {}
+
+        void DeleteAtPos(int Pos)
+        {}
+
+        void Display()
+        {}
+
+        int Count()
+        {
+            return iCount;
+        }
+};
+
+int main()
+{
+    DoublyCL dobj;
+    int iRet = 0;
+
+    dobj.InsertFirst(51);
+    dobj.InsertFirst(21);
+    dobj.InsertFirst(11);
+
+    return 0;
+}
