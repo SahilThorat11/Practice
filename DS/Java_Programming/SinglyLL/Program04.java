e0a694a4b9dc8340b9648876c89ad32f412a3448
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-18 13:44:54 +0530

    InsertLast function

---
+++ DS/Java_Programming/SinglyLL/Program04.java
@@ -0,0 +1,129 @@
+class SinglyLLNode
+{
+    public int data;
+    public SinglyLLNode next;
+
+    public SinglyLLNode(int no)
+    {
+        data = no;
+        next = null;
+    }
+}
+
+class SinglyLL
+{
+    private SinglyLLNode first;
+    private int iCount;
+
+    public SinglyLL()
+    {
+        System.out.println("Object is created successfully");
+        first = null;
+        iCount = 0;
+    }
+
+    public void InsertFirst(int no)
+    {
+        SinglyLLNode newn = null;
+
+        newn = new SinglyLLNode(no);
+
+        newn.next = first;
+        first = newn;
+
+        iCount++;
+    }
+
+    public void InsertLast(int no)
+    {
+        SinglyLLNode newn = null;
+        SinglyLLNode temp = null;
+
+        newn = new SinglyLLNode(no);
+
+        if(first == null)
+        {
+            first = newn;
+        }
+        else
+        {
+            temp = first;
+
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = newn;
+        }
+
+        iCount++;
+    }
+
+    public void InsertAtPos(int no, int Pos)
+    {
+
+    }
+
+    public void DeleteFirst()
+    {
+
+    }
+
+    public void DeleteLast()
+    {
+
+    }
+
+    public void DeleteAtPos(int Pos)
+    {
+        
+    }
+
+    public void Display()
+    {
+        SinglyLLNode temp = null;
+
+        temp = first;
+
+        System.out.println();
+
+        while(temp != null)
+        {
+            System.out.print("| " + temp.data + " | -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public int Count()
+    {
+        return iCount;
+    }
+}
+
+class Program04
+{
+    public static void main(String A[])
+    {
+        SinglyLL sobj = new SinglyLL();
+        int iRet = 0;
+
+        sobj.InsertFirst(51);
+        sobj.InsertFirst(21);
+        sobj.InsertFirst(11);
+
+        sobj.Display();
+        iRet = sobj.Count();
+        System.out.println("Number of nodes are : " + iRet);
+
+        sobj.InsertLast(101);
+        sobj.InsertLast(111);
+        sobj.InsertLast(121);
+
+        sobj.Display();
+        iRet = sobj.Count();
+        System.out.println("Number of nodes are : " + iRet);
+    }
+}
