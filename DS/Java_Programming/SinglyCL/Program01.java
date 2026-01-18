b340e39c2a7e0ea103df0b0d168608fb48367a87
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-18 15:35:45 +0530

    Node Creation and Function Declaration

---
+++ DS/Java_Programming/SinglyCL/Program01.java
@@ -0,0 +1,74 @@
+class SinglyCLNode
+{
+    public int data;
+    public SinglyCLNode next;
+
+    public SinglyCLNode(int no)
+    {
+        data = no;
+        next = null;
+    }
+}
+
+class SinglyCL
+{
+    public SinglyCLNode first;
+    public SinglyCLNode last;
+    public int iCount;
+
+    public SinglyCL()
+    {
+        first = null;
+        last = null;
+        iCount = 0;
+    }
+
+    public void InsertFirst(int no)
+    {
+
+    }
+
+    public void InsertLast(int no)
+    {
+        
+    }
+
+    public void InsertAtPos(int no, int Pos)
+    {
+        
+    }
+
+    public void DeleteFirst()
+    {
+
+    }
+
+    public void DeleteLast()
+    {
+        
+    }
+
+    public void DeleteAtPos()
+    {
+        
+    }
+
+    public void Display()
+    {
+
+    }
+
+    public int Count()
+    {
+        return iCount;
+    }
+
+}
+
+class Program01
+{
+    public static void main(String A[])
+    {
+        
+    }
+}
