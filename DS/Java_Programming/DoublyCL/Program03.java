8024e2ae8db801ae6579234790d231a2d0e4dcc6
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-19 16:29:25 +0530

    Display and Count Function

---
+++ DS/Java_Programming/DoublyCL/Program03.java
@@ -0,0 +1,121 @@
+class DoublyCLLNode
+{
+    public int data;
+    public DoublyCLLNode next;
+    public DoublyCLLNode prev;
+
+    public DoublyCLLNode(int no)
+    {
+        data = no;
+        next = null;
+        prev = null;
+    }
+}
+
+class DoublyCLL
+{
+    private DoublyCLLNode first;
+    private DoublyCLLNode last;
+    private int iCount;
+
+    public DoublyCLL()
+    {
+        this.first = null;
+        this.last = null;
+        this.iCount = 0;
+    }
+
+    public void InsertFirst(int no)
+    {
+        DoublyCLLNode newn = null;
+
+        newn = new DoublyCLLNode(no);
+
+        if((first == null) && (last == null))
+        {
+            first = newn;
+            last = newn;
+        }
+        else
+        {
+            newn.next = first;
+            first.prev = newn;
+            first = newn;
+        }
+
+        first.prev = last;
+        last.next = first;
+
+        iCount++;
+    }
+
+    public void InsertLast(int no)
+    {
+        
+    }
+
+    public void InsertAtPos(int no, int Pos)
+    {
+        
+    }
+
+    public void DeleteFirst()
+    {
+        
+    }
+
+    public void DeleteLast()
+    {
+    
+    }
+
+    public void DeleteAtPos(int Pos)
+    {
+      
+    }
+
+    public void Display()
+    {
+        DoublyCLLNode temp = null;
+
+        temp = first;
+
+        if((first == null) && (last == null))
+        {
+            System.out.println("Linked list is Empty");
+            return;
+        }
+
+        System.out.print("\n<=> ");
+
+        do
+        {
+            System.out.print("| " + temp.data + " | <=> ");
+            temp = temp.next;
+        }while(temp != first);
+
+        System.out.println();
+    }
+
+    public int Count()
+    {
+        return iCount;
+    }
+}
+
+class DoublyCL
+{
+    public static void main(String A[])
+    {
+        DoublyCLL dobj = new DoublyCLL();
+        int iRet = 0;
+
+        dobj.Display();
+        iRet = dobj.Count();
+        System.out.println("Number of nodes are : " + iRet);
+
+        dobj.InsertFirst(51);
+        dobj.InsertFirst(21);
+        dobj.InsertFirst(11);        
+    }
+}
