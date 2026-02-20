4aff28eda6525ff1eeab8f465c8827c573f3dda2
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-20 12:57:20 +0530

    Program for Sorting using Insertion Sort

---
+++ DS/C++_Programming/Sorting/Insertion_Sort/Program01.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+        bool Sorted;
+
+        // Constructor, Destructor
+        ArrayX(int no);
+        ~ArrayX();
+
+        // Accept the values and Display the values
+        void Accept();
+        void Display();
+
+        bool CheckSorted();
+
+        void InsertionSort();
+};
+
+ArrayX :: ArrayX(int no)
+{
+    cout<<"Inside constructor\n";
+    iSize = no;
+    Arr = new int[iSize];
+    Sorted = true;
+}
+
+ArrayX :: ~ArrayX()
+{
+    cout<<"Inside destructor\n";
+    delete [] Arr;
+}
+
+void ArrayX :: Accept()            // Time Complexity N. It will sort while getting the input
+{  
+    int iCnt = 0;
+
+    cout<<"Enter the elements : \n";
+
+    cin >> Arr[iCnt];               // Get first input outside the loop
+
+    for(iCnt = 1; iCnt < iSize; iCnt++) // Start loop from 1st index
+    {
+        cin >> Arr[iCnt];
+
+        if(Arr[iCnt - 1] > Arr[iCnt])
+        {
+            Sorted = false;
+        }
+    }
+
+    // Sorted = CheckSorted();
+}
+
+void ArrayX :: Display()
+{
+    int iCnt = 0;
+
+    cout<<"Elemets of the array are : \n";
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        cout<<Arr[iCnt]<<"\t";
+    }
+
+    cout<<"\n";
+}
+
+// 0    1   2   3   4   5   6   7   8   9
+// 12   14  17  24  28  36  48  52  52  65
+bool ArrayX :: CheckSorted()
+{
+    int i = 0;
+    bool bFlag = true;
+
+    for (i = 0; i < iSize - 1; i++)
+    {
+        if(Arr[i] > Arr[i + 1])
+        {
+            bFlag = false;
+            break;
+        }
+    }
+
+    return bFlag;
+}
+
+void ArrayX :: InsertionSort()
+{
+    int i = 0, j = 0, Key = 0;
+
+    for (i = 1; i < iSize; i++)
+    {
+        Key = Arr[i];
+        j = i - 1;
+
+        while(j >= 0 && Arr[j] > Key)
+        {
+            Arr[j + 1] = Arr[j];
+            j = j - 1;
+        }
+
+        Arr[j + 1] = Key;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter the number of elements : \n";
+    cin>>iValue;
+
+    ArrayX aobj(iValue);
+
+    aobj.Accept();
+
+    cout << "\nData Before Sorting :-\n";
+    aobj.Display();
+
+    if(aobj.Sorted == true)
+    {
+        cout << "\nData is sorted.\n";
+    }
+    else
+    {
+        cout << "\nData is not sorted.\n";
+    }
+
+    aobj.InsertionSort();
+
+    cout << "\nData After Sorting :-\n";
+    aobj.Display();
+
+    return 0;
+}
